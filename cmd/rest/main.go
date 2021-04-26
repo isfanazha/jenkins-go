7e07c7b2f93312722b3c5d448fbff87209b528e0
Isfan Azhabil <isfanazhabil@gmail.com>
2021-04-26 20:51:52 +0700

    Implement simple rest API using echo

--- cmd/rest/main.go
+++ cmd/rest/main.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
 
-func main()  {
-	fmt.Println("ok")
+	"github.com/labstack/echo"
+)
+
+func main() {
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
